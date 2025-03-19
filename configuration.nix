cff3c4bc8a1f5436573a357e0d08e696fa2d7c67
Sebastien Chagnon <sebastien.chagnon@proton.me>
2025-03-18 20:31:47 -0400

    wip

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./modules/shell.nix
+    ./modules/desktop.nix
   ];
 
   boot.loader.efi.canTouchEfiVariables = true;
@@ -29,72 +30,9 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
-  fonts.enableDefaultPackages = true;
-  fonts.packages = with pkgs; [
-    ubuntu_font_family
-    liberation_ttf
-    vazir-fonts
-    font-awesome
-    nerd-fonts.fira-code
-  ];
-  fonts.fontconfig = {
-    defaultFonts = {
-      serif = [  "Liberation Serif" "Vazirmatn" ];
-      sansSerif = [ "Ubuntu" "Vazirmatn" ];
-      monospace = [ "FiraCode" ];
-    };
-  };
-
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
   ];
-
-  hardware.graphics.enable = true;
-  hardware.nvidia-container-toolkit.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    forceFullCompositionPipeline = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-    open = false;
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
-	user = "greeter";
-      };
-    };
-    restart = true;
-  };
-
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
-
-  services.xserver.xkb.layout = "ca";
-  services.xserver.xkb.variant = "fr";
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  services.seatd.enable = true;
 
   virtualisation.docker = {
     enable = true;
@@ -116,37 +54,7 @@
 
   home-manager.users.seb = import ./home/home.nix;
 
-  environment.systemPackages = with pkgs; [
-    kdePackages.dolphin
-    kdePackages.breeze-icons
-    spacedrive
-    seatd
-    pavucontrol
-    swaynotificationcenter # Notification daemon 
-    playerctl
-    cudatoolkit
-  ];
-
   programs.nix-ld.enable = true;
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.variables = {
-    XDG_SESSION_TYPE = "wayland";
-    XDG_RUNTIME_DIR = "/run/user/$(id -u)";
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
 
   nix.gc = {
     automatic = true;
