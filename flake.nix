3f0e3880974fd5480402dfff2631b927cd06ffce
Sebastien Chagnon <sebastien.chagnon@qohash.com>
2025-03-19 13:35:19 +0000

    wsl

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +14,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim }:
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, nixvim }:
     let
       system = "x86_64-linux";
     in {
@@ -33,6 +34,33 @@
                   nixvim.homeManagerModules.nixvim 
                 ];
                 users.seb = ./home/home.nix;
+              };
+            }
+          ];
+        };
+        WSL = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            nixos-wsl.nixosModules.default
+
+            {
+              wsl.enable = true;
+              wsl.defaultUser = "seb";
+              system.stateVersion = "24.11";
+            }
+
+            ./generic.nix
+            ./modules/shell.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                sharedModules = [
+                  nixvim.homeManagerModules.nixvim 
+                ];
+                users.seb = ./home/wsl.nix;
               };
             }
           ];
---
+++ generic.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./modules/shell.nix
+  ];
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
+
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [];
+  };
+
+  programs.nix-ld.enable = true;
+
+  time.timeZone = "America/Montreal";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.defaultUserShell = pkgs.zsh;
+
+}
